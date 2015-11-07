607fdb7597371c881b68b17bcace4170981ccbf8
Noah Manion <noah.manion@gmail.com>
2015-11-06 23:41:40 -0600

    secret's out

--- config1.js
+++ config1.js
@@ -1,6 +1,6 @@
 module.exports = {
     consumer_key: 'WV0RQbs8IuEk85pVZ7epGM3bo'
-  , consumer_secret: '3xH7pb460YbAMzwP1FwDdm1KJXSN2FdJWAYPsEmYmOFkBQcfsB'
-  , access_token: '3015923299-KZcwraFxRON2dDr7RirSXvmie0jPtoDA49lNF0r'
-  , access_token_secret: 'V2GH6ayjY7b1vqkjUzqPGQubColKiT0lfvgx5tXGEC5SB'
+  , consumer_secret: nope
+ , access_token: '3015923299-KZcwraFxRON2dDr7RirSXvmie0jPtoDA49lNF0r'
+  , access_token_secret: nuh uh
 }
